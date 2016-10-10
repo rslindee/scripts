85c3dcfd055f64d435d49c71a1158ca5c73af7ef
Richard Slindee <katzinator@gmail.com>
2016-10-09 18:55:57 -0700

    Initial commit of helper scripts

---
+++ flac2mp3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+FLAC_DIR=/mnt/hit1/FLAC/_Incoming
+MP3_DIR=/mnt/hit1/MP3/
+# Enter root FLAC incoming directory
+cd "$FLAC_DIR"
+for d in */ ; do
+    # Enter FLAC album dir
+	cd "$d"
+    # Create an album directory in MP3 root dir
+	mkdir "$MP3_DIR/$d"
+    # Re-encode FLAC to MP3
+	parallel-moreutils -i -j$(nproc) ffmpeg -i {} -qscale:a 0 {}.mp3 -- ./*.flac
+	rename .flac.mp3 .mp3 ./*.mp3
+    # Move MP3 files to album dir in MP3 directory
+	mv *.mp3 "$MP3_DIR/$d"
+	cd "$FLAC_DIR"
+    # Move FLAC album folder out of Incoming and into FLAC root folder
+	mv "$d" ..
+done
---
+++ post_download_sort.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+{
+    FLAC_DIR=/mnt/hit1/FLAC/_Incoming
+    MP3_DIR=/mnt/hit1/MP3/_Non_FLAC
+    MKV_DIR=/mnt/3Tera/Movies
+    LOG_FILE=/mnt/BLUE500/download_log.txt
+    echo "========================================"
+    echo "$(date) ---- $TR_TORRENT_NAME finished"
+    cd "$TR_TORRENT_DIR"
+    # If torrent is dir, enter dir
+    if [ -d "$TR_TORRENT_NAME" ]
+    then
+        # Check for FLAC and copy
+		if ls "$TR_TORRENT_NAME"/*.flac > /dev/null 2>&1
+		then
+            echo "FLAC found"
+			cp -av "$TR_TORRENT_NAME" "$FLAC_DIR"
+		elif ls "$TR_TORRENT_NAME"/*.mp3 > /dev/null 2>&1
+        then
+            echo "MP3 found"
+			cp -av "$TR_TORRENT_NAME" "$MP3_DIR"
+		elif ls "$TR_TORRENT_NAME"/*.mkv > /dev/null 2>&1
+        then
+            echo "MKV(s) in folder found"
+			cp -av "$TR_TORRENT_NAME"/*.mkv "$MKV_DIR"
+		fi
+    else
+        case "${TR_TORRENT_NAME}" in
+            *mkv)
+                echo "MKV found"
+                cp -av "$TR_TORRENT_NAME" "$MKV_DIR" ;;
+        esac
+    fi
+} >> "LOG_FILE"
