fd2f0a12626ac26f14d660c815909ec5d0359e82
Richard Slindee <katzinator@gmail.com>
2016-10-09 19:01:34 -0700

    Comments and formatting

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# Re-encodes FLAC folders to MP3
 FLAC_DIR=/mnt/hit1/FLAC/_Incoming
 MP3_DIR=/mnt/hit1/MP3/
 # Enter root FLAC incoming directory
 cd "$FLAC_DIR"
 for d in */ ; do
     # Enter FLAC album dir
-	cd "$d"
+    cd "$d"
     # Create an album directory in MP3 root dir
-	mkdir "$MP3_DIR/$d"
+    mkdir "$MP3_DIR/$d"
     # Re-encode FLAC to MP3
-	parallel-moreutils -i -j$(nproc) ffmpeg -i {} -qscale:a 0 {}.mp3 -- ./*.flac
-	rename .flac.mp3 .mp3 ./*.mp3
+    parallel-moreutils -i -j$(nproc) ffmpeg -i {} -qscale:a 0 {}.mp3 -- ./*.flac
+    rename .flac.mp3 .mp3 ./*.mp3
     # Move MP3 files to album dir in MP3 directory
-	mv *.mp3 "$MP3_DIR/$d"
-	cd "$FLAC_DIR"
+    mv *.mp3 "$MP3_DIR/$d"
+    cd "$FLAC_DIR"
     # Move FLAC album folder out of Incoming and into FLAC root folder
-	mv "$d" ..
+    mv "$d" ..
 done
--- post_download_sort.sh
+++ post_download_sort.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Sorts media files when finished downloading via Transmission
 {
     FLAC_DIR=/mnt/hit1/FLAC/_Incoming
     MP3_DIR=/mnt/hit1/MP3/_Non_FLAC
@@ -11,19 +12,19 @@
     if [ -d "$TR_TORRENT_NAME" ]
     then
         # Check for FLAC and copy
-		if ls "$TR_TORRENT_NAME"/*.flac > /dev/null 2>&1
-		then
+        if ls "$TR_TORRENT_NAME"/*.flac > /dev/null 2>&1
+        then
             echo "FLAC found"
-			cp -av "$TR_TORRENT_NAME" "$FLAC_DIR"
-		elif ls "$TR_TORRENT_NAME"/*.mp3 > /dev/null 2>&1
+            cp -av "$TR_TORRENT_NAME" "$FLAC_DIR"
+        elif ls "$TR_TORRENT_NAME"/*.mp3 > /dev/null 2>&1
         then
             echo "MP3 found"
-			cp -av "$TR_TORRENT_NAME" "$MP3_DIR"
-		elif ls "$TR_TORRENT_NAME"/*.mkv > /dev/null 2>&1
+            cp -av "$TR_TORRENT_NAME" "$MP3_DIR"
+        elif ls "$TR_TORRENT_NAME"/*.mkv > /dev/null 2>&1
         then
             echo "MKV(s) in folder found"
-			cp -av "$TR_TORRENT_NAME"/*.mkv "$MKV_DIR"
-		fi
+            cp -av "$TR_TORRENT_NAME"/*.mkv "$MKV_DIR"
+        fi
     else
         case "${TR_TORRENT_NAME}" in
             *mkv)
