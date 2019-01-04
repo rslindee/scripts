29a4230b8636557160206b570826ac0041c14150
Richard Slindee <rslindee@bird.co>
2019-01-03 22:35:42 -0800

    status: add improved battery and lines

--- status.sh
+++ status.sh
@@ -1,20 +1,60 @@
 #!/bin/sh
-# try https://unix.stackexchange.com/questions/436886/command-to-get-details-on-charge-and-time-remaining-for-two-batteries
-# use acpitool
-#count="$(acpi -b | wc -l)"
-count=1
+get_time_remaining() {
+    # parses acpitool's battery info for the remaining charge of all batteries and sums them up
+    sum_remaining_charge=$(acpitool -B | grep -E 'Remaining capacity' | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc);
+    # finds the rate at which the batteries being drained at
+    present_rate=$(acpitool -B | grep -E 'Present rate' | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc);
+    # divides current charge by the rate at which it's falling, then converts it into seconds for `date`
+    seconds=$(bc <<< "scale = 10; ($sum_remaining_charge / $present_rate) * 3600");
+    # prettifies the seconds into h:mm:ss format
+    pretty_time=$(date -u -d @${seconds} +%R);
 
-sum="$(acpi -b | egrep -o '[0-9]{1,3}%' | tr -d '%' | xargs -I% echo -n '+%')"
-battery="Avg capacity: $(( sum / count ))%"
+    echo $pretty_time;
+}
+
+get_battery_combined_percent() {
+    # get charge of all batteries, combine them
+    total_charge=$(expr $(acpi -b | awk '{print $4}' | grep -Eo "[0-9]+" | paste -sd+ | bc));
+    # get amount of batteries in the device
+    battery_number=$(acpi -b | wc -l);
+    percent=$(expr $total_charge / $battery_number);
+
+    echo $percent;
+}
+
+get_battery_charging_status() {
+    # acpi can give Unknown or Charging if charging, https://unix.stackexchange.com/questions/203741/lenovo-t440s-battery-status-unknown-but-charging
+    if ! $(acpi -b | grep --quiet Discharging)
+    then
+        echo "CHRG";
+    else
+        echo "BATT";
+    fi
+}
+
+if [ -e /sys/class/power_supply/BAT0 ]; then
+    battery="
+    $(get_battery_charging_status) $(get_battery_combined_percent)% $(get_time_remaining)"
+else
+    battery=""
+fi
 
 date="$(date '+%m/%d/%y %H:%M')"
-wifi_ssid="$(nmcli --colors no -terse -field NAME connection show --active)"
-wifi_rssi="$(cat /proc/net/wireless | awk '/wlp/ {print $4}' | cut -d . -f 1)dBm"
-audio="♪: $(amixer sget 'Master' | awk '/Front Left:/ {print $5 " " $6}' | tr -d '[]')"
-statusinfo=\
-"$wifi_ssid \
-$wifi_rssi \
-$audio \
-$battery"
 
-notify-send "$date" "$statusinfo"
+audio="♪: $(amixer sget 'Master' | awk '/Front Left:/ {print $5 " " $6}' | tr -d '[]')"
+if [ ! -z "$audio" ]; then
+    statusinfo+="$audio"
+fi
+wifi_ssid="$(nmcli -color no -field type,state,connection dev status | awk '/^wifi.*connected/ {print $3}')"
+wifi_rssi="$(cat /proc/net/wireless | awk '/^wl/ {print $4}' | cut -d . -f 1)dBm"
+if [ ! -z "$wifi_ssid" ]; then
+    statusinfo+="
+$wifi_ssid $wifi_rssi"
+fi
+
+if [ -e /sys/class/power_supply/BAT0 ]; then
+    statusinfo+="
+$(get_battery_charging_status) $(get_battery_combined_percent)% $(get_time_remaining)"
+fi
+
+notify-send --urgency=low "$date" "$statusinfo"
