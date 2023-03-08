a5901e90ce4a74ba4322478d43dcdf4701132eea
Richard Slindee <richard@slindee.net>
2023-03-07 21:53:43 -0800

    flac2mp3: Fixes while loop bug. removes unneeded steps

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -2,8 +2,7 @@
 # Re-encodes FLAC folders to MP3
 # dependencies: ffmpeg, flac
 FLAC_INCOMING_ALBUM_DIR=$1
-FLAC_DIR=$2
-MP3_DIR=$3
+MP3_DIR=$2
 
 # Exit prematurely on any failure
 set -e
@@ -11,13 +10,13 @@ set -e
 # Gets metadata from FLAC file and populate $ALBUM_FOLDER
 get_artist_album ()
 {
-    ARTIST=$(metaflac --show-tag=ARTIST "$1")
+    ARTIST=$(metaflac --show-tag=ARTIST "$1" | sed -n 1p)
     ARTIST=${ARTIST#*=}
     # Remove forward slahes
     ARTIST=$(sed "s/\//\_/g" <<< "$ARTIST")
     # Convert spaces to underscores
     ARTIST=${ARTIST// /_}
-    ALBUM=$(metaflac --show-tag=ALBUM "$1")
+    ALBUM=$(metaflac --show-tag=ALBUM "$1" | sed -n 1p)
     ALBUM=${ALBUM#*=}
     ALBUM=$(sed "s/\//\_/g" <<< "$ALBUM")
     ALBUM=${ALBUM// /_}
@@ -26,27 +25,24 @@ get_artist_album ()
 # If albumartist is null, then copy over artist tag
 set_albumartist_if_null ()
 {
-    ALBUMARTIST=$(metaflac --show-tag=ALBUMARTIST "$1")
+    ALBUMARTIST=$(metaflac --show-tag=ALBUMARTIST "$1" | sed -n 1p)
     ALBUMARTIST=${ALBUMARTIST#*=}
     if [ -z "$ALBUMARTIST" ]; then
-        TEMP_ARTIST=$(metaflac --show-tag=ARTIST "$1")
+        TEMP_ARTIST=$(metaflac --show-tag=ARTIST "$1" | sed -n 1p)
         TEMP_ARTIST=${TEMP_ARTIST#*=}
-        metaflac --set-tag=ALBUMARTIST="$TEMP_ARTIST" "$1"
+        # metaflac --set-tag=ALBUMARTIST="$TEMP_ARTIST" "$1"
     fi
 }
 
 # Enter FLAC album directory
 cd "$FLAC_INCOMING_ALBUM_DIR"
 
-find . -iname '*.flac' -type f -print0 | while IFS= read -r -d '' file
-do
-    # If albumartist is null, then copy over artist tag
-    set_albumartist_if_null "$file"
-    # Get artist and album name from first FLAC file
-    get_artist_album "$file"
-    break
-done
-
+# Recursively search for a flac file
+FIRST_FILE=$(find . -iname '*.flac' -type f -print -quit)
+# If albumartist is null, then copy over artist tag
+set_albumartist_if_null "$FIRST_FILE"
+# Get artist and album name from first FLAC file
+get_artist_album "$FIRST_FILE"
 # Re-encode FLAC to MP3
 find . -iname '*.flac' -type f -print0 | while IFS= read -r -d '' file
 do
@@ -59,14 +55,3 @@ find . -iname '*.mp3' -type f -print0 | while IFS= read -r -d '' file
 do
   mv "$file" "$MP3_DIR/$ARTIST/$ALBUM"
 done
-# Create directory in FLAC root dir
-mkdir -p "$FLAC_DIR/$ARTIST/$ALBUM"
-# Move remaining flacs and data to new FLAC dir
-find . -iname '*.flac' -type f -print0 | while IFS= read -r -d '' file
-do
-  mv "$file" "$FLAC_DIR/$ARTIST/$ALBUM"
-done
-# Delete (now empty) FLAC album folder
-rm -rf "$FLAC_INCOMING_ALBUM_DIR"
-# Add flac location to mpd library
-#mpc update "$FLAC_DIR/$ARTIST/$ALBUM"
